4f571f3b83c677201984d4a66be15199b7336016
William Barela <william.barela.dev@gmail.com>
2017-04-20 00:34:37 +0900

    added article class and several aritcle methods

---
+++ lib/article.rb
@@ -0,0 +1,20 @@
+class Article
+  attr_accessor # add attributes which are expected from article hash
+  @@all = []
+
+  def initialize(article_hash)
+
+  end
+
+  def self.create_from_collection(article_array)
+
+  end
+
+  def add_article_attributes(attributes_hash)
+
+  end
+
+  def self.all
+    @@all
+  end
+end
