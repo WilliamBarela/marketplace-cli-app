85394b9906e7772620a9f3ba24e31611a1c947c5
William Barela <william.barela.dev@gmail.com>
2017-04-19 23:54:22 +0900

    added required libraries for scraping

---
+++ lib/marketplace.rb
@@ -0,0 +1,7 @@
+require_relative "./marketplace/version"
+
+require "open-uri"
+require "nokogiri"
+require "net_http_ssl_fix" #necessary only for Windows because of https problem in RUby
+
+#require_relative "./marketplace/cli"
