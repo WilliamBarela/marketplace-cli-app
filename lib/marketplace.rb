41aebd612000284ff5efb89a93102cec00967059
William Barela <william.barela.dev@gmail.com>
2017-04-20 01:09:09 +0900

    added required files and libraries to marketplace executable and environment

--- lib/marketplace.rb
+++ lib/marketplace.rb
@@ -1,5 +1,8 @@
 require_relative "./marketplace/version"
+require_relative "./article"
+require_relative "./scraper"
 
+require "pry"
 require "open-uri"
 require "nokogiri"
 require "net_http_ssl_fix" #necessary only for Windows because of https problem in RUby
