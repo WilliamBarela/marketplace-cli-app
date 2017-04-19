e586ab5b9b8c078a8a5162f240c252ffefc67c49
William Barela <william.barela.dev@gmail.com>
2017-04-20 01:28:12 +0900

    working on making scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,8 @@
 class Scraper
 
-  def self.scrape_home_page(home_url)
-
+  def self.scrape_home_page(home_url="https://www.marketplace.org/")
+    page = Scraper.sashimi(home_url)
+    #articles = page.css("article .feat-1")
   end
 
   def self.scrape_article(article_url)
