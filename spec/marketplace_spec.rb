f6e9661fd722d8bfcdab1e5f869efcfa826514aa
William Barela <william.barela.dev@gmail.com>
2017-04-20 00:16:15 +0900

    removed spec item which is from gem bundle initialization

--- spec/marketplace_spec.rb
+++ spec/marketplace_spec.rb
@@ -5,7 +5,7 @@ describe Marketplace do
     expect(Marketplace::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  # it "does something useful" do
+  #   expect(false).to eq(true)
+  # end
 end
