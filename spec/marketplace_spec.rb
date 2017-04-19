5dc47280e7f7018ceeef4d02d30d09ba9a5db210
William Barela <william.barela.dev@gmail.com>
2017-04-19 23:35:20 +0900

    added spec files

---
+++ spec/marketplace_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe Marketplace do
+  it "has a version number" do
+    expect(Marketplace::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
