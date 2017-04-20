786b0ed422708cb506b1ce66dd0ea887f1b69d1b
William Barela <william.barela.dev@gmail.com>
2017-04-20 11:45:17 +0900

    added outline of hashes to collect for each article

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,14 +2,23 @@ class Scraper
 
   def self.scrape_home_page(home_url="https://www.marketplace.org/")
     page = Scraper.sashimi(home_url)
-    #articles = page.css("article .feat-1")
+    articles = page.css(".river-collection .feat-1")
+    articles.collect do |article|
+      {}.tap do |hash|
+        hash[:headline] =
+        hash[:authors] =
+        hash[:date] =
+        hash[:teaser] =
+        hash[:link] =
+      end
+    end
   end
 
   def self.scrape_article(article_url)
 
   end
 
-  def self.sashimi(url="https://www.marketplace.org/")
+  def self.sashimi(url)
     Nokogiri::HTML(open(url).read, nil, 'utf-8')
   end
 end
