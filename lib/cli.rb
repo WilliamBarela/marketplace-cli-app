1a2665ecefa769abdfa34c3858e479cfaf93b952
William Barela <william.barela.dev@gmail.com>
2017-04-20 16:28:54 +0900

    added skeleton methods to run CLI

---
+++ lib/cli.rb
@@ -0,0 +1,9 @@
+class Cli
+  def pull_articles
+    # should scrape marketplace and create the articles
+  end
+
+  def add_articles_info
+    # should use the linkes provided in the Arists.all[i].link to to scrape paragraphs and audio_link
+  end
+end
