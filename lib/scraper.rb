3b1c48de6c0908291fce59607ae8db504de5f4f4
William Barela <william.barela.dev@gmail.com>
2017-04-20 12:23:50 +0900

    completed Scraper.scrape_home_page method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,11 +5,11 @@ class Scraper
     articles = page.css(".river-collection .feat-1")
     articles.collect do |article|
       {}.tap do |hash|
-        hash[:headline] =
-        hash[:authors] =
-        hash[:date] =
-        hash[:teaser] =
-        hash[:link] =
+        hash[:headline] = article.css(".river--hed a").text
+        hash[:authors] = article.css(".river-byline").css("a").collect{|author| author.text}
+        hash[:date] = article.css(".river--time").text
+        hash[:teaser] = article.css(".dek").text
+        hash[:link] = articles[2].css(".river--hed a").attr("href").value
       end
     end
   end
