fee6eb78ad39c8aa93b90446e48a1794d2e01701
William Barela <william.barela.dev@gmail.com>
2017-04-21 12:07:04 +0900

    added #display_index_page to support single responsibility principle

--- lib/cli.rb
+++ lib/cli.rb
@@ -42,38 +42,48 @@ class Cli
   end
 
   def list
+    self.display_index_page
+    puts "\nPlease select an article to read"
+
+    second_input = ""
+    until second_input == "exit"
+      second_input = gets.strip.downcase
+      case second_input
+        when /rl (\d+)/
+          self.clear
+          i = second_input.match(/rl (\d+)/)[1].to_i - 1
+          article = self.articles[i]
+          puts article.headline.colorize(:white).on_green.bold
+          article.paragraphs.each {|p| puts " #{p}\n\n"}
+          system(%Q[chrome --app="data:text/html,<html><body><script>window.moveTo(20,20);window.resizeTo(700,250);window.location='#{article.audio_link}';</script></body></html>"])
+          puts "\n\nPlease type #{'list'.colorize(:red)} to go back"
+        when /(l|listen) (\d+)/
+          #fill me out
+        when /(r|read) (\d+)/
+          #fill me out
+        else
+          self.display_index_page
+          puts "Please enter a valid command, eg: " + "read 4".colorize(:red) + ", " +
+               "listen 3".colorize(:red) + ", " +
+               "r 4".colorize(:red) + ", or "  +
+               "l 3".colorize(:red)
+      end
+    end
+  end
+
+  def display_index_page
     self.clear
     puts "Marketplace Top Stories of the Day\n\n".colorize(:white).on_green.bold
 
     self.articles.each_with_index do |article, i|
       puts " #{(i + 1).to_s} ".colorize(:white).on_red.bold + " " + article.headline.colorize(:blue).bold
-      puts "     |_--#{article.teaser}\n\n"
-    end
-    puts "\nPlease select an article to read"
-    second_input = gets.strip.downcase
-    case second_input
-      when /rl (\d+)/
-        self.clear
-        i = second_input.match(/rl (\d+)/)[1].to_i - 1
-        article = self.articles[i]
-        puts article.headline.colorize(:white).on_green.bold
-        article.paragraphs.each {|p| puts " #{p}\n\n"}
-        system(%Q[chrome --app="data:text/html,<html><body><script>window.moveTo(20,20);window.resizeTo(700,250);window.location='#{article.audio_link}';</script></body></html>"])
-        puts "\n\nPlease type #{'list'.colorize(:red)} to go back"
-      when /(l|listen) (\d+)/
-        #fill me out
-      when /(r|read) (\d+)/
-        #fill me out
-      else
-        puts "Please enter a valid command, eg: " + "read 4".colorize(:red) + ", " +
-             "listen 3".colorize(:red) + ", " +
-             "r 4".colorize(:red) + ", or "  +
-             "l 3".colorize(:red)
+      puts "     |_--#{article.teaser}"
+      puts "     |_--#{"READ".colorize(:white).on_yellow.bold} #{(article.audio_link != nil) ? "LISTEN".colorize(:white).on_magenta.bold : ""}\n\n"
     end
   end
 
   def man
-
+    puts "this is the man"
   end
 
   def about
